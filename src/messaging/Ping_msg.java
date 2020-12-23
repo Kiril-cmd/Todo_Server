cefc4bc03e2197e38a5dd546294dcd5372e70107
Kiril <Kiril@DESKTOP-96DEBKH>
2020-12-23 17:50:52 +0100

    Ping

--- src/messaging/Ping_msg.java
+++ src/messaging/Ping_msg.java
@@ -3,9 +3,17 @@ package messaging;
 public class Ping_msg extends Message {
 	private String token;
 	
+	public Ping_msg() {
+		super(MessageType.PING);
+	}
+	
 	public Ping_msg(String token) {
 		super(MessageType.PING);
 		this.token = token;
 	}
 
+	@Override
+	public String toString() {
+		return type.toString();
+	}
 }
