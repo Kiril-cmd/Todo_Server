caca3e6f9970c06c9737c57e9f3bb1cd1c7e3d71
Kiril <Kiril@DESKTOP-96DEBKH>
2020-12-23 15:28:31 +0100

    Todo attributes

---
+++ src/userData/ToDo.java
@@ -0,0 +1,22 @@
+package userData;
+
+import java.time.LocalDate;
+
+public class ToDo {
+	private int id;
+	private static int counter = -1;
+	private String title;
+	private enum Priority { LOW, MEDIUM, HIGH }
+	private Priority priority;
+	private String description;
+	private LocalDate dueDate;
+	
+	
+	public ToDo (String title, Priority priority, String description, LocalDate dueDate) {
+		this.id = ++counter;
+		this.title = title;
+		this.priority = priority;
+		this.description = description;
+		this.dueDate = dueDate;
+	}
+}
