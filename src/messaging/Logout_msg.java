4f6e6a970157b864ac1d0ab6e0252979c6373797
Kiril <Kiril@DESKTOP-96DEBKH>
2020-12-23 17:30:14 +0100

    msg types

--- src/messaging/Logout_msg.java
+++ src/messaging/Logout_msg.java
@@ -1,5 +1,13 @@
 package messaging;
 
-public class Logout_msg {
-
+public class Logout_msg extends Message{
+	
+	public Logout_msg() {
+		super(MessageType.LOGOUT);
+	}
+	
+	@Override
+	public String toString() {
+		return type.toString();
+	}
 }
