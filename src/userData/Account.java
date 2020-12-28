62e663f1616097055fce4ce53a621a69c9e53f67
Leandro <Leandro@DESKTOP-HK6U5DG.fritz.box>
2020-12-28 17:05:59 +0100

    Token implemented in Client class. Token removed from Account class.

--- src/userData/Account.java
+++ src/userData/Account.java
@@ -5,25 +5,24 @@ import java.util.ArrayList;
 public class Account {
 	private String userName;
 	private String password;
-	private final String token;
 	private ArrayList<ToDo> toDoList;
 	
 	
 	public Account (String userName, String password){
 		this.userName = userName;
 		this.password = password;
-		this.token = generateToken();
 		this.toDoList = new ArrayList<>();
 	}
 	
-	private static String generateToken() {
-		int stringSize = 20;
-		String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-		String token = "";
-		for (int i = 0; i < stringSize; i++) {
-			char letter = characters.charAt((int) (Math.random() * characters.length()));
-			token = token + letter;
-		}
-		return token;
+	public String getUserName() {
+		return userName;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
 	}
 }
